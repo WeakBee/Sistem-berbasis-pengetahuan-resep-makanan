8f6e55d7ef509c451e5d827f2c4c937917f56ac0
Ichwanul Muslim <82445431+WeakBee@users.noreply.github.com>
2022-10-21 09:03:38 +0700

    push resep oleh

--- script.js
+++ script.js
@@ -1,53 +1,203 @@
-$.getJSON('./API/umami.json', function (data){
-    // Membuat Kartu
-    $.each(data, function(i,data){
-        $('.all-resep').append(`
-        <div class="col-4">
-            <div class="list-resep">
-                <div class="d-flex align-items-center">
-                    <img class="foto-makanan" src="./assets/Sayur Asem.jpg">
-                    <div> 
-                        <p class="fw-bold">`+ data.nama +`</p>
-                        <p>Umami</p>
+
+function umami(){
+    $(".sortbytags").text("Umami");
+    $('.all-resep').empty();
+    $.getJSON('./API/umami.json', function (data){
+        // Membuat Kartu
+        $.each(data, function(i,data){
+            $('.all-resep').append(`
+            <div class="col-4">
+                <div class="list-resep">
+                    <div class="d-flex align-items-center">
+                        <img class="foto-makanan" src="./assets/Sayur Asem.jpg">
+                        <div> 
+                            <p class="fw-bold">`+ data.nama +`</p>
+                            <p>Umami</p>
+                        </div>
+                    </div>
+                    <div class="my-3">
+                        <p class="mb-3 fw-bold">Bahan Masakan :</p>
+                        <ul class="semua-bahan resep-`+ i +`">
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <button class="mulai-masak">
+                            Mulai Masak
+                            <img src="./assets/icon/fire.svg">
+                        </button>
+                        <img src="./assets/icon/Time.svg">
+                        <span class="waktu-masak">15 Menit</span>
                     </div>
                 </div>
-                <div class="my-3">
-                    <p class="mb-3 fw-bold">Bahan Masakan :</p>
-                    <ul class="semua-bahan resep-`+ i +`">
-                        <li>Air</li>
-                    </ul>
-                </div>
-                <div class="d-flex align-items-center">
-                    <button class="mulai-masak">
-                        Mulai Masak
-                        <img src="./assets/icon/fire.svg">
-                    </button>
-                    <img src="./assets/icon/Time.svg">
-                    <span>15 Menit</span>
-                </div>
             </div>
-        </div>
-        `);
-    });
-    // Membuat Kartu
-
-    // Jumlah data
-    var jumlah = data.length;
-    // Jumlah data
-    
-    // Masukin data bahan
-    for (let a = 0; a < jumlah; a++) {
-        var datas = data[a];
-        $.each(datas.semua_bahan, function(i,data){
-            $('.resep-'+a).append(`
-                <li>`+ data.bahan +`</li>
             `);
         });
-        // $.each(datas.cara, function(i,data){
-        //     $('.cara-'+a).append(`
-        //         <li>`+ data.resep +`</li>
-        //     `);
-        // });
-    }
-    // Masukin data bahan
-});
+        // Membuat Kartu
+    
+        // Jumlah data
+        var jumlah = data.length;
+        // Jumlah data
+        
+        // Masukin data bahan
+        for (let a = 0; a < jumlah; a++) {
+            var datas = data[a];
+            $.each(datas.semua_bahan, function(i,data){
+                $('.resep-'+a).append(`
+                    <li>`+ data.bahan +`</li>
+                `);
+            });
+        }
+        // Masukin data bahan
+    });
+}
+function royco(){
+    $(".sortbytags").text("Royco");
+    $('.all-resep').empty();
+    $.getJSON('./API/royco.json', function (data){
+        // Membuat Kartu
+        $.each(data, function(i,data){
+            $('.all-resep').append(`
+            <div class="col-4">
+                <div class="list-resep">
+                    <div class="d-flex align-items-center">
+                        <img class="foto-makanan" src="./assets/Sayur Asem.jpg">
+                        <div> 
+                            <p class="fw-bold">`+ data.nama +`</p>
+                            <p>Royco</p>
+                        </div>
+                    </div>
+                    <div class="my-3">
+                        <p class="mb-3 fw-bold">Bahan Masakan :</p>
+                        <ul class="semua-bahan resep-`+ i +`">
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <button class="mulai-masak">
+                            Mulai Masak
+                            <img src="./assets/icon/fire.svg">
+                        </button>
+                        <img src="./assets/icon/Time.svg">
+                        <span class="waktu-masak">15 Menit</span>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+        // Membuat Kartu
+    
+        // Jumlah data
+        var jumlah = data.length;
+        // Jumlah data
+        
+        // Masukin data bahan
+        for (let a = 0; a < jumlah; a++) {
+            var datas = data[a];
+            $.each(datas.semua_bahan, function(i,data){
+                $('.resep-'+a).append(`
+                    <li>`+ data.bahan +`</li>
+                `);
+            });
+        }
+        // Masukin data bahan
+    });
+}
+function roycoumami(){
+    $(".sortbytags").text("Semua");
+    $('.all-resep').empty();
+    $.getJSON('./API/royco.json', function (data){
+        // Membuat Kartu
+        $.each(data, function(i,data){
+            $('.all-resep').append(`
+            <div class="col-4">
+                <div class="list-resep">
+                    <div class="d-flex align-items-center">
+                        <img class="foto-makanan" src="./assets/Sayur Asem.jpg">
+                        <div> 
+                            <p class="fw-bold">`+ data.nama +`</p>
+                            <p>Royco</p>
+                        </div>
+                    </div>
+                    <div class="my-3">
+                        <p class="mb-3 fw-bold">Bahan Masakan :</p>
+                        <ul class="semua-bahan resep-`+ i +`">
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <button class="mulai-masak">
+                            Mulai Masak
+                            <img src="./assets/icon/fire.svg">
+                        </button>
+                        <img src="./assets/icon/Time.svg">
+                        <span class="waktu-masak">15 Menit</span>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+        // Membuat Kartu
+    
+        // Jumlah data
+        var jumlah = data.length;
+        // Jumlah data
+        
+        // Masukin data bahan
+        for (let a = 0; a < jumlah; a++) {
+            var datas = data[a];
+            $.each(datas.semua_bahan, function(i,data){
+                $('.resep-'+a).append(`
+                    <li>`+ data.bahan +`</li>
+                `);
+            });
+        }
+        // Masukin data bahan
+    });
+    $.getJSON('./API/umami.json', function (data){
+        // Membuat Kartu
+        $.each(data, function(i,data){
+            $('.all-resep').append(`
+            <div class="col-4">
+                <div class="list-resep">
+                    <div class="d-flex align-items-center">
+                        <img class="foto-makanan" src="./assets/Sayur Asem.jpg">
+                        <div> 
+                            <p class="fw-bold">`+ data.nama +`</p>
+                            <p>Umami</p>
+                        </div>
+                    </div>
+                    <div class="my-3">
+                        <p class="mb-3 fw-bold">Bahan Masakan :</p>
+                        <ul class="semua-bahan resep-`+ i +`">
+                        </ul>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <button class="mulai-masak">
+                            Mulai Masak
+                            <img src="./assets/icon/fire.svg">
+                        </button>
+                        <img src="./assets/icon/Time.svg">
+                        <span class="waktu-masak">15 Menit</span>
+                    </div>
+                </div>
+            </div>
+            `);
+        });
+        // Membuat Kartu
+    
+        // Jumlah data
+        var jumlah = data.length;
+        // Jumlah data
+        
+        // Masukin data bahan
+        for (let a = 0; a < jumlah; a++) {
+            var datas = data[a];
+            $.each(datas.semua_bahan, function(i,data){
+                $('.resep-'+a).append(`
+                    <li>`+ data.bahan +`</li>
+                `);
+            });
+        }
+        // Masukin data bahan
+    });
+}
+
+roycoumami();
